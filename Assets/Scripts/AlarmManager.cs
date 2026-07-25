9017007f58191fadd94218c8facdf2cf0980084e
Marc-André <marc-andre.bouvrette@hotmail.com>
2026-07-25 10:10:39 -0400

    alarm

--- Assets/Scripts/AlarmManager.cs
+++ Assets/Scripts/AlarmManager.cs
@@ -1,22 +1,42 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AlarmManager : MonoBehaviour {
+    [SerializeField] private Image alarmLight;
+    [SerializeField] private float flashSpeed = 3f;
+    [SerializeField] private float minAlpha = 0.1f;
+    [SerializeField] private float maxAlpha = 0.5f;
+
     private AudioSource alarmSound;
 
     private void Awake() {
         alarmSound = GetComponent<AudioSource>();
         alarmSound.Stop();
+
+        SetAlpha(0f);
     }
 
     private void Update() {
-        if (FailuresManager.Instance.IsSomethingBroken()) {
-            if (!alarmSound.isPlaying) {
+        bool isBroken = FailuresManager.Instance.IsSomethingBroken();
+
+        if (isBroken) {
+            if (!alarmSound.isPlaying)
                 alarmSound.Play();
-            }
+
+            float t = Mathf.PingPong(Time.time * flashSpeed, 1f);
+            float alpha = Mathf.Lerp(minAlpha, maxAlpha, t);
+            SetAlpha(alpha);
         } else {
-            if (alarmSound.isPlaying) {
+            if (alarmSound.isPlaying)
                 alarmSound.Stop();
-            }
+
+            SetAlpha(0f);
         }
+    }
+
+    private void SetAlpha(float alpha) {
+        Color color = alarmLight.color;
+        color.a = alpha;
+        alarmLight.color = color;
     }
 }
