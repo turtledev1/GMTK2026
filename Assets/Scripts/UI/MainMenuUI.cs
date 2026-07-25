ccf7904b7df181d1f926bcfbcab828404b5f5124
Marc-André <marc-andre.bouvrette@hotmail.com>
2026-07-25 12:09:37 -0400

    main menu

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -3,14 +3,14 @@ using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour {
     [SerializeField] private Button playButton;
+    [SerializeField] private Button quitButton;
 
     private void Awake() {
         playButton.onClick.AddListener(() => {
             SceneLoader.Load(SceneLoader.Scene.Game);
         });
-    }
-
-    private void Start() {
-        Debug.Log(UpAndRunningSystems.GetPermanentlyBrokenSystemsByPriority().Count);
+        quitButton.onClick.AddListener(() => {
+            Application.Quit();
+        });
     }
 }
